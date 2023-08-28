f0b1ec288db79d9399e1eaba3b6323bc0bee8f2b
Alona <alona.costa2@gmail.com>
2023-08-28 13:24:23 +0100

    Fix error edit expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -70,7 +70,7 @@ export const startEditExpense = (id, updates) => {
   return (dispatch, getState) => {
     const uid = getState().auth.uid;
     return database
-      .ref(`users/${uid}/expenses`)
+      .ref(`users/${uid}/expenses/${id}`)
       .update(updates)
       .then(() => {
         dispatch(editExpense(id, updates));
